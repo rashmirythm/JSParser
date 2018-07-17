ff9e6b2d7fd312f60119a30b85646345cab8b70e
i338008 <rashmi.r02@sap.com>
2018-07-17 09:42:19 +0530

    added a js file

---
+++ Calculation.js
@@ -0,0 +1,30 @@
+
+	function add(a, b) {
+	    return a + b;            // Function returns the product of a and b
+	}
+
+	function sub(a,b)
+	{
+		return a - b;
+		}
+
+	function pro(a,b)
+	{
+		return a * b;
+		}
+
+	function quo(a,b)
+	{
+		return a / b;
+		}
+
+	function mod(a,b)
+	{
+		var a = 5;
+		var b = 10;
+		return a % b;
+		}
+	
+	
+
+
