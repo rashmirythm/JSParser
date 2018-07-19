d6767823cfd1e4057d227c80e0dcafb3d5a1ed5b
i338008 <rashmi.r02@sap.com>
2018-07-19 11:42:36 +0530

    added new js file

---
+++ JSsamplefunctions.js
@@ -0,0 +1,49 @@
+function fun1()
+{
+	var a=5;
+	var b=5;
+	var c=a+b;
+	return c;
+	
+	}
+
+function fun2()
+{
+	var a=5;
+	var b=5;
+	var c=a+b;
+	return c;
+	
+	}
+function fun3()
+{
+	var a=5;
+	var b=5;
+	var c=a+b;
+	return c;
+	
+	}
+function fun4()
+{
+	var a=5;
+	var b=5;
+	var c=a+b;
+	return c;
+	
+	}
+function fun5()
+{
+	var a=5;
+	var b=5;
+	var c=a+b;
+	return c;
+	
+	}
+function fun6()
+{
+	var a=5;
+	var b=5;
+	var c=a+b;
+	return c;
+	
+	}
