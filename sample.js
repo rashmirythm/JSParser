527efb767284b9c3ab2a8cac4a8b5d4016b6bbf2
i338008 <rashmi.r02@sap.com>
2018-07-30 16:28:42 +0530

    one more file added

---
+++ sample.js
@@ -0,0 +1,22 @@
+function fun1()
+{
+	var a=5;
+	var b=5;
+	var c=a+b;
+	return c;
+	
+	}
+
+function add()
+{
+	var a=0;
+	var b=0;
+	return c;
+	
+	var a=0;
+	var b=0;
+	return c;
+	var a=0;
+	var b=0;
+	return c;
+}
