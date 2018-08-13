b1908f7549aeaa4cc10d446f06c8ca256f805300
i338008 <rashmi.r02@sap.com>
2018-08-13 17:26:50 +0530

    added new js file

---
+++ jsfile.js
@@ -0,0 +1,23 @@
+function fun2(a,b,c)
+{
+	var a=5;
+	var b=5;
+	var c=a+b;
+	var d=a-b;
+	var sqr=a*a;
+	return c;
+	
+	}
+function fun3()
+{
+	var a=5;
+	var b=5;
+	var c=a+b;
+	var d=6;
+	var e=c+d;
+	return c;
+	var a=0;
+	var b=0;
+	return c;
+	
+	}
